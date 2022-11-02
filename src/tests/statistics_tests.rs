052cd4aa63af2200ccc919aa6808af3772beb3ff
DmitryAstafyev <dvastafyev@gmail.com>
2022-11-02 11:57:36 +0100

    Update test for statistics

--- src/tests/statistics_tests.rs
+++ src/tests/statistics_tests.rs
@@ -19,19 +19,19 @@ mod tests {
     fn get_stat_entities() -> Vec<(String, LevelDistribution)> {
         vec![
             (
-                String::from("Debug"),
+                String::from("ID_0"),
                 LevelDistribution::new(Some(LogLevel::Debug)),
             ),
             (
-                String::from("Error"),
+                String::from("ID_1"),
                 LevelDistribution::new(Some(LogLevel::Error)),
             ),
             (
-                String::from("Verbose"),
+                String::from("ID_2"),
                 LevelDistribution::new(Some(LogLevel::Verbose)),
             ),
             (
-                String::from("Warn"),
+                String::from("ID_3"),
                 LevelDistribution::new(Some(LogLevel::Warn)),
             ),
         ]
@@ -60,13 +60,30 @@ mod tests {
         assert_eq!(stat_b.app_ids[2].1.log_verbose, 1);
         assert_eq!(stat_b.app_ids[3].1.log_warning, 1);
         stat_a.merge(stat_b);
-        assert_eq!(stat_a.app_ids[0].0, String::from("Debug"));
-        assert_eq!(stat_a.app_ids[1].0, String::from("Error"));
-        assert_eq!(stat_a.app_ids[2].0, String::from("Verbose"));
-        assert_eq!(stat_a.app_ids[3].0, String::from("Warn"));
+        assert_eq!(stat_a.app_ids[0].0, String::from("ID_0"));
+        assert_eq!(stat_a.app_ids[1].0, String::from("ID_1"));
+        assert_eq!(stat_a.app_ids[2].0, String::from("ID_2"));
+        assert_eq!(stat_a.app_ids[3].0, String::from("ID_3"));
         assert_eq!(stat_a.app_ids[0].1.log_debug, 2);
         assert_eq!(stat_a.app_ids[1].1.log_error, 2);
         assert_eq!(stat_a.app_ids[2].1.log_verbose, 2);
         assert_eq!(stat_a.app_ids[3].1.log_warning, 2);
+        assert_eq!(stat_a.context_ids[0].0, String::from("ID_0"));
+        assert_eq!(stat_a.context_ids[1].0, String::from("ID_1"));
+        assert_eq!(stat_a.context_ids[2].0, String::from("ID_2"));
+        assert_eq!(stat_a.context_ids[3].0, String::from("ID_3"));
+        assert_eq!(stat_a.context_ids[0].1.log_debug, 2);
+        assert_eq!(stat_a.context_ids[1].1.log_error, 2);
+        assert_eq!(stat_a.context_ids[2].1.log_verbose, 2);
+        assert_eq!(stat_a.context_ids[3].1.log_warning, 2);
+        assert_eq!(stat_a.ecu_ids[0].0, String::from("ID_0"));
+        assert_eq!(stat_a.ecu_ids[1].0, String::from("ID_1"));
+        assert_eq!(stat_a.ecu_ids[2].0, String::from("ID_2"));
+        assert_eq!(stat_a.ecu_ids[3].0, String::from("ID_3"));
+        assert_eq!(stat_a.ecu_ids[0].1.log_debug, 2);
+        assert_eq!(stat_a.ecu_ids[1].1.log_error, 2);
+        assert_eq!(stat_a.ecu_ids[2].1.log_verbose, 2);
+        assert_eq!(stat_a.ecu_ids[3].1.log_warning, 2);
+        assert_eq!(stat_a.contained_non_verbose, true);
     }
 }
